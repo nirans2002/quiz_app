927e5dfcfc2d3477b390130f7b46150dd9260d93
Niran S Narayanan <nirans2002@gmail.com>
2021-11-20 08:09:54 +0530

    splash

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,15 +1,39 @@
-
 import 'package:flutter/material.dart';
+import 'package:quiz_app/screens/home_screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    gohome();
+    super.initState();
+  }
+
+  Future<void> gohome() async {
+    // navigates to the home page after provided time of 2 seconds
+    await Future.delayed(const Duration(seconds: 2));
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (cntxt) => const HomeScreen(),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Splash Screen'),
+    return const Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Text("splash screen"),
+          //TODO: add logo
+        ),
+      ),
     );
   }
 }
-
-
